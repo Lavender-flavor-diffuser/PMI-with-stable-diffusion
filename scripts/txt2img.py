d28485a7eef5cede8e219f544b1f13c1c2b79482
yjdong9697 <yjdong9697@yonsei.ac.kr>
2024-10-08 17:56:48 +0900

    Intermediate tensor update

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -315,9 +315,8 @@ def main():
                                                          unconditional_conditioning=uc,
                                                          eta=opt.ddim_eta,
                                                          x_T=start_code)
-                        # 중간 이미지 텐서를 저장
-                        save_intermediate(intermediate, sample_path, base_count)
                         
+                        ## Decoding Latent Representations to Images (이전까지는 latent space 상에서의 tensor)
                         x_samples_ddim = model.decode_first_stage(samples_ddim)
                         x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
                         x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
@@ -326,8 +325,24 @@ def main():
 
                         x_checked_image_torch = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
 
-                        # 최종 이미지 텐서를 저장
-                        save_final_tensor(x_checked_image_torch, sample_path, base_count)
+                        # Initialize an empty list to collect tensors
+                        intermediate_tensor = []
+
+                        # Simulate a loop where tensors are generated
+                        for i in range(len(intermediate)):
+                            tensor = intermediate['x_inter'][i]
+                            tensor_samples_ddim = model.decode_first_stage(tensor)
+                            tensor_samples_ddim = torch.clamp((tensor_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+                            tensor_samples_ddim = tensor_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
+
+                            tensor_checked_image, has_nsfw_concept = check_safety(tensor_samples_ddim)
+
+                            tensor_checked_image_torch = torch.from_numpy(tensor_checked_image).permute(0, 3, 1, 2)
+                            intermediate_tensor.append(tensor_checked_image_torch)
+                        intermediate_tensor = torch.cat(intermediate_tensor)
+                        
+                        # 최종 이미지 텐서를 저장 (처음, 중간과정(log_exp단위마다), 맨 끝 이미지)
+                        save_intermediate(intermediate_tensor, sample_path, base_count)
                         
                         if not opt.skip_save:
                             for x_sample in x_checked_image_torch:
